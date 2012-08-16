234f5587cfa3c2be9e834e63e3f7505bda3f27b6
James Ward <james@jamesward.org>
2012-08-16 10:43:33 -0600

    Switch to Postgres in on Heroku

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,8 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "com.amazonaws" % "aws-java-sdk" % "1.3.11"
+      "com.amazonaws" % "aws-java-sdk" % "1.3.11",
+      "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
