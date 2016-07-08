80f6e0bb642de83d89ecd0bee062de275cdcec20
Joe Kutner <jpkutner@gmail.com>
2016-07-08 08:55:09 -0500

    Update Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       "com.amazonaws" % "aws-java-sdk" % "1.3.11",
-      "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
+      "postgresql" % "postgresql" % "9.4-1201-jdbc4"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
